47c92a447321b356241f26f39fb234fd128bfb77
stephen <54988149+sharoskyy@users.noreply.github.com>
2020-11-28 04:50:12 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Head>
         <title>nexs.ro</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <Navbar />
       </Head>
       <Welcome />
